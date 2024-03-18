275e66f4cc3d128b43aac07bbb0c53c0416ca6b1
Smirnova Tanya <75753895+SmirnovaT@users.noreply.github.com>
2024-03-18 18:19:51 +0300

    Initial commit

---
+++ sqlite_to_postgres/load_data.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+import psycopg2
+from psycopg2.extensions import connection as _connection
+from psycopg2.extras import DictCursor
+
+
+def load_from_sqlite(connection: sqlite3.Connection, pg_conn: _connection):
+    """Основной метод загрузки данных из SQLite в Postgres"""
+    # postgres_saver = PostgresSaver(pg_conn)
+    # sqlite_extractor = SQLiteExtractor(connection)
+
+    # data = sqlite_extractor.extract_movies()
+    # postgres_saver.save_all_data(data)
+
+
+if __name__ == '__main__':
+    dsl = {'dbname': 'movies_database', 'user': 'app', 'password': '123qwe', 'host': '127.0.0.1', 'port': 5432}
+    with sqlite3.connect('db.sqlite') as sqlite_conn, psycopg2.connect(**dsl, cursor_factory=DictCursor) as pg_conn:
+        load_from_sqlite(sqlite_conn, pg_conn)
